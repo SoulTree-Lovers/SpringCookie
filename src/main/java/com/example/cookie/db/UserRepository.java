dd5d70575375089d5fb39b69f06ac84e943f00e0
kangseungmin <rkdtmdals999@naver.com>
2024-06-04 09:45:26 +0900

    feat: UserDtoList print 메소드 추가

--- src/main/java/com/example/cookie/db/UserRepository.java
+++ src/main/java/com/example/cookie/db/UserRepository.java
@@ -6,12 +6,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserRepository {
 
     private final List<UserDto> userDtoList = new ArrayList<>();
+
+    public void printUserDtoList() {
+        userDtoList.stream()
+            .forEach(System.out::println);
+    }
 
     public Optional<UserDto> findById(String id) {
         return userDtoList.stream()
@@ -31,8 +39,11 @@ public class UserRepository {
 
     @PostConstruct
     public void init() {
+        log.info("사용자 자동 생성");
         userDtoList.add(new UserDto(UUID.randomUUID().toString(), "강승민", "1234"));
         userDtoList.add(new UserDto(UUID.randomUUID().toString(), "최민석", "1234"));
         userDtoList.add(new UserDto(UUID.randomUUID().toString(), "강진호", "1234"));
+
+        printUserDtoList();
     }
 }
